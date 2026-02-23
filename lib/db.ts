dd52038c22adb8e441d9a79d98f13376569facd9
patidarsumit707-glitch <patidarsumit707@gmail.com>
2026-02-23 10:07:13 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/paytrack";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://patidarsumit707_db_user:LjQJYo4AQkNh3vyK@cluster0.ebme6qb.mongodb.net/?appName=Cluster0";
 
 declare global {
   var mongooseConn: Promise<typeof mongoose> | undefined;
